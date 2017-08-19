4557cc3ad28298a3eb18182ad92013ea77dc63e5
Martin Disch <martindisch@gmail.com>
2017-08-19 13:33:16 +0200

    Switch from EditTexts to TextViews for SeekBar status

--- app/src/main/java/com/martindisch/chronoscopy/SettingsActivity.java
+++ app/src/main/java/com/martindisch/chronoscopy/SettingsActivity.java
@@ -5,11 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.EditText;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
 
     private SeekBar mSbRegret, mSbSkill, mSbFun, mSbResponsibility;
-    private EditText mEtRegret, mEtSkill, mEtFun, mEtResponsibility, mEtLeisure, mEtDob;
+    private EditText mEtLeisure, mEtDob;
+    private TextView mTvRegret, mTvSkill, mTvFun, mTvResponsibility;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +29,10 @@ public class SettingsActivity extends AppCompatActivity implements SeekBar.OnSee
         mSbSkill = (SeekBar) findViewById(R.id.settings_sbSkill);
         mSbFun = (SeekBar) findViewById(R.id.settings_sbFun);
         mSbResponsibility = (SeekBar) findViewById(R.id.settings_sbResponsibility);
-        mEtRegret = (EditText) findViewById(R.id.settings_etRegret);
-        mEtSkill = (EditText) findViewById(R.id.settings_etSkill);
-        mEtFun = (EditText) findViewById(R.id.settings_etFun);
-        mEtResponsibility = (EditText) findViewById(R.id.settings_etResponsibility);
+        mTvRegret = (TextView) findViewById(R.id.settings_tvRegretStatus);
+        mTvSkill = (TextView) findViewById(R.id.settings_tvSkillStatus);
+        mTvFun = (TextView) findViewById(R.id.settings_tvFunStatus);
+        mTvResponsibility = (TextView) findViewById(R.id.settings_tvResponsibilityStatus);
         mEtLeisure = (EditText) findViewById(R.id.settings_etLeisure);
         mEtDob = (EditText) findViewById(R.id.settings_etDate);
 
@@ -45,16 +47,16 @@ public class SettingsActivity extends AppCompatActivity implements SeekBar.OnSee
     public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
         switch (seekBar.getId()) {
             case R.id.settings_sbRegret:
-                mEtRegret.setText(String.valueOf(seekBar.getProgress()));
+                mTvRegret.setText(String.valueOf(seekBar.getProgress()));
                 break;
             case R.id.settings_sbSkill:
-                mEtSkill.setText(String.valueOf(seekBar.getProgress()));
+                mTvSkill.setText(String.valueOf(seekBar.getProgress()));
                 break;
             case R.id.settings_sbFun:
-                mEtFun.setText(String.valueOf(seekBar.getProgress()));
+                mTvFun.setText(String.valueOf(seekBar.getProgress()));
                 break;
             case R.id.settings_sbResponsibility:
-                mEtResponsibility.setText(String.valueOf(seekBar.getProgress()));
+                mTvResponsibility.setText(String.valueOf(seekBar.getProgress()));
                 break;
         }
     }
