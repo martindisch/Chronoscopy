54fb41613c61c8dafe2c8c0b257efe416c2f92d8
Martin Disch <martindisch@gmail.com>
2017-08-20 15:44:25 +0200

    Make the tab indicator white instead of accent color

--- app/src/main/java/com/martindisch/chronoscopy/MainActivity.java
+++ app/src/main/java/com/martindisch/chronoscopy/MainActivity.java
@@ -1,23 +1,22 @@
 package com.martindisch.chronoscopy;
 
 import android.content.Intent;
-import android.support.design.widget.TabLayout;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -45,6 +44,10 @@ public class MainActivity extends AppCompatActivity {
         // Set icons
         tabLayout.getTabAt(0).setIcon(R.drawable.ic_activities);
         tabLayout.getTabAt(1).setIcon(R.drawable.ic_history);
+        // Set tab indicator color to white because the accent color wouldn't stand out enough
+        tabLayout.setSelectedTabIndicatorColor(
+                ContextCompat.getColor(this, android.R.color.white)
+        );
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
