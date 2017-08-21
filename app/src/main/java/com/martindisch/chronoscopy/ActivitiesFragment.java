6981937053e0674637523bed4594f82fb858fbda
Martin Disch <martindisch@gmail.com>
2017-08-21 19:31:15 +0200

    Add empty activities Fragment

---
+++ app/src/main/java/com/martindisch/chronoscopy/ActivitiesFragment.java
@@ -0,0 +1,29 @@
+package com.martindisch.chronoscopy;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ActivitiesFragment extends Fragment {
+
+    public ActivitiesFragment() {
+        // Required empty public constructor
+    }
+
+    public static ActivitiesFragment newInstance() {
+        Log.e("FFF", "Recreating");
+        return new ActivitiesFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_activities, container, false);
+    }
+
+}
