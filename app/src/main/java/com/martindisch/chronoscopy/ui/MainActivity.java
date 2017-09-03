51dc5c6d5af55996c1857e1870c9b14901b9bfd3
Martin Disch <martindisch@gmail.com>
2017-09-03 15:57:11 +0200

    Remove unused imports

--- app/src/main/java/com/martindisch/chronoscopy/ui/MainActivity.java
+++ app/src/main/java/com/martindisch/chronoscopy/ui/MainActivity.java
@@ -11,12 +11,9 @@ import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.martindisch.chronoscopy.R;
 import com.orm.SugarContext;
