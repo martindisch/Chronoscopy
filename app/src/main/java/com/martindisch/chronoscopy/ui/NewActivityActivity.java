a1945132899d121d01e9e2b746ee4f5cce9a47b5
Martin Disch <martindisch@gmail.com>
2017-09-03 15:17:07 +0200

    Fix for duration pattern

--- app/src/main/java/com/martindisch/chronoscopy/ui/NewActivityActivity.java
+++ app/src/main/java/com/martindisch/chronoscopy/ui/NewActivityActivity.java
@@ -29,7 +29,7 @@ public class NewActivityActivity extends AppCompatActivity implements SeekBar.On
     private TextInputLayout mTilDate, mTilTime;
 
     private static final String mDatePattern = "^(?:[1-9]\\d{3}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1\\d|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[1-9]\\d(?:0[48]|[2468][048]|[13579][26])|(?:[2468][048]|[13579][26])00)-02-29)$";
-    private static final String mTimePattern = "^(\\d+:)?[0-5]?\\d$";
+    private static final String mTimePattern = "^(\\d+:[0-5]\\d)|([0-5]?\\d)$";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
