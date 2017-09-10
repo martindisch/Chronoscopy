c11b4bf3d34c25b6928913127403df426c7d6625
Martin Disch <martindisch@gmail.com>
2017-09-10 14:49:00 +0200

    Calculate and display today's score and spent time

--- app/src/main/java/com/martindisch/chronoscopy/ui/ActivitiesFragment.java
+++ app/src/main/java/com/martindisch/chronoscopy/ui/ActivitiesFragment.java
@@ -8,18 +8,23 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.martindisch.chronoscopy.R;
 import com.martindisch.chronoscopy.logic.ChrActivity;
 import com.martindisch.chronoscopy.logic.ChrIndividual;
+import com.martindisch.chronoscopy.logic.ChrUsage;
 import com.martindisch.chronoscopy.logic.Util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class ActivitiesFragment extends Fragment {
 
     private RecyclerView mRvActivities;
     private ActivityAdapter mAdapter;
+    private TextView mTvScore, mTvTime, mTvEvaluation;
 
     public ActivitiesFragment() {
         // Required empty public constructor
@@ -36,6 +41,9 @@ public class ActivitiesFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_activities, container, false);
         // Get views
         mRvActivities = (RecyclerView) view.findViewById(R.id.activities_rvActivities);
+        mTvScore = (TextView) view.findViewById(R.id.activities_tvToday);
+        mTvTime = (TextView) view.findViewById(R.id.activities_tvSpent);
+        mTvEvaluation = (TextView) view.findViewById(R.id.activities_tvEvaluation);
 
         // Prepare RecyclerView
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
@@ -66,6 +74,30 @@ public class ActivitiesFragment extends Fragment {
                     @Override
                     public void run() {
                         mRvActivities.setAdapter(mAdapter);
+                    }
+                });
+
+                // Read today's usages from DB
+                String today = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+                List<ChrUsage> usages = ChrUsage.find(ChrUsage.class, "date = ?", today);
+                // Get spent time for the day
+                final String todayTime = Util.getTimeTotal(usages);
+                // Get score for the day
+                double cumulScore = 0;
+                for (ChrUsage usage : usages) {
+                    // Get the activity for the usage
+                    ChrActivity activity = ChrActivity.findById(
+                            ChrActivity.class, usage.getActivityId());
+                    cumulScore += individual.getScore(activity, usage.getTimeHours());
+                }
+                final double todayScore = cumulScore;
+                // Update status texts for today
+                mTvScore.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mTvScore.setText(String.format("%.1f", todayScore));
+                        mTvTime.setText(todayTime);
+                        mTvEvaluation.setText("0 / 5");
                     }
                 });
             }
