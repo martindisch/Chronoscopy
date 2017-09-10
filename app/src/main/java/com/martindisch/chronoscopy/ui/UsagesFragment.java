3c125626199e8a829235fb07a3b79c289a92113f
Martin Disch <martindisch@gmail.com>
2017-09-10 19:52:18 +0200

    Add ItemTouchHelper for deleting a usage by swiping

--- app/src/main/java/com/martindisch/chronoscopy/ui/UsagesFragment.java
+++ app/src/main/java/com/martindisch/chronoscopy/ui/UsagesFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +21,7 @@ public class UsagesFragment extends Fragment {
 
     private RecyclerView mRvUsages;
     private UsageAdapter mAdapter;
+    private List<ChrUsage> mUsages;
 
     public UsagesFragment() {
         // Required empty public constructor
@@ -44,6 +46,29 @@ public class UsagesFragment extends Fragment {
         mRvUsages.addItemDecoration(new DividerItemDecoration(getActivity(),
                 layoutManager.getOrientation()));
         mRvUsages.setHasFixedSize(true);
+        ItemTouchHelper.SimpleCallback swipeCallback = new ItemTouchHelper.SimpleCallback(
+                0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
+                                  RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                int position = viewHolder.getAdapterPosition();
+                // Get usage that was swiped away
+                ChrUsage toDelete = mUsages.get(position);
+                // Delete the usage from DB
+                ChrUsage.delete(toDelete);
+                // Remove the usage from the list used by the adapter
+                mUsages.remove(position);
+                // Notify adapter of removal
+                mRvUsages.getAdapter().notifyItemRemoved(position);
+            }
+        };
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(swipeCallback);
+        itemTouchHelper.attachToRecyclerView(mRvUsages);
         return view;
     }
 
@@ -55,12 +80,12 @@ public class UsagesFragment extends Fragment {
             @Override
             public void run() {
                 // Read usages from DB
-                List<ChrUsage> usages = ChrUsage.find(
+                mUsages = ChrUsage.find(
                         ChrUsage.class, null, null, null, "date DESC, id DESC", null
                 );
                 // Build ChrIndividual from SharedPreferences
                 ChrIndividual individual = Util.getIndividual(getContext());
-                mAdapter = new UsageAdapter(usages, individual);
+                mAdapter = new UsageAdapter(mUsages, individual);
                 // Update RecyclerView in UI thread
                 mRvUsages.post(new Runnable() {
                     @Override
