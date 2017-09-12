0087fdf0496f026020c971472c65bd1a5524724e
Martin Disch <martindisch@gmail.com>
2017-09-12 21:18:30 +0200

    Implement building of JSON with just the data

--- app/src/main/java/com/martindisch/chronoscopy/ui/SettingsActivity.java
+++ app/src/main/java/com/martindisch/chronoscopy/ui/SettingsActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +15,13 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.martindisch.chronoscopy.R;
+import com.martindisch.chronoscopy.logic.ChrActivity;
+import com.martindisch.chronoscopy.logic.ChrEvaluation;
+import com.martindisch.chronoscopy.logic.ChrUsage;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class SettingsActivity extends AppCompatActivity
         implements SeekBar.OnSeekBarChangeListener, TextWatcher, View.OnClickListener {
@@ -130,7 +138,54 @@ public class SettingsActivity extends AppCompatActivity
      */
     @Override
     public void onClick(View view) {
-        // TODO
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    JSONObject activities = new JSONObject();
+                    JSONArray usages = new JSONArray();
+                    // Go through all activities
+                    for (ChrActivity activity : ChrActivity.listAll(ChrActivity.class)) {
+                        // Collect properties of the activity
+                        JSONObject currentActivity = new JSONObject();
+                        currentActivity.put("regret", activity.getRegret());
+                        currentActivity.put("skill", activity.getSkill());
+                        currentActivity.put("fun", activity.getFun());
+                        // Add the activity to activities
+                        activities.put(activity.getName(), currentActivity);
+
+                        // Go through all usages of the activity
+                        for (ChrUsage usage : ChrUsage.find(
+                                ChrUsage.class, "activity_id = ?", activity.getId() + "")) {
+                            // Collect properties of the usage
+                            JSONObject currentUsage = new JSONObject();
+                            currentUsage.put("activity", activity.getName());
+                            currentUsage.put("date", usage.getDate());
+                            currentUsage.put("time", usage.getTime());
+                            // Add the usage to usages
+                            usages.put(currentUsage);
+                        }
+                    }
+
+                    JSONObject evaluations = new JSONObject();
+                    // Go through all evaluations
+                    for (ChrEvaluation evaluation : ChrEvaluation.listAll(ChrEvaluation.class)) {
+                        // Add the evaluation to evaluations
+                        evaluations.put(evaluation.getDate(), evaluation.getRating());
+                    }
+
+                    // Build collection of all data
+                    JSONObject data = new JSONObject();
+                    data.put("activities", activities);
+                    data.put("usages", usages);
+                    data.put("evaluations", evaluations);
+                    Log.e("FFF", data.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
     }
 
     @Override
